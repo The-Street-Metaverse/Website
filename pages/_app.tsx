6c1cbca22e431656f82036582ced77b183beb7d0
Antonis Valamontes <tony@geopay.me>
2022-06-14 00:28:21 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,12 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <StatusProvider>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+       </Head>
+    <Component {...pageProps} />
+    </StatusProvider>
     </>
   )
 }
